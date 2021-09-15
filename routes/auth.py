f5ab568fcc82244b5e4237046f62945312d75ad3
Independent-Dev <juju08217@daum.net>
2021-09-15 23:26:36 +0900

    이메일 인증 기능 구현   - 인증 메일 발송 및 인증 완료 페이지 및 처리 로직

--- routes/auth.py
+++ routes/auth.py
@@ -9,7 +9,7 @@ from monolithic import db
 from monolithic.models.users import User
 from monolithic.forms.auth_form import LoginForm, RegisterForm
 from monolithic.utils.common import flash_form_errors
-from monolithic.utils.user import send_auth_email
+from monolithic.utils.user import send_auth_email, confirm_email_token_status
 
 
 NAME = 'auth'
@@ -24,14 +24,17 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(user_email=form.user_email.data).first()
         if user:
+            # TODO 이 로직도 not과 else를 쓰지 않는 방식으로 변경할 수 있을 듯
             if not security.check_password_hash(user.password, form.password.data):
                 flash('password is wrong')
             else:
+                if not user.active:
+                    return render_template(f'{NAME}/register_email_info_page.html', user_email=user.user_email)
                 login_user(user)
                 return redirect(url_for('data.file'))
         else:
             flash('account not exists')
-            return redirect(request.path)
+            return redirect(request.path)  # TODO 이게 꼭 필요할 것인가...
 
     return render_template(f'{NAME}/login.html', form=form)
 
@@ -42,32 +45,65 @@ def logout():
 
 @bp.route('/register/', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('data.file'))
+
     form = RegisterForm()
-    # todo 먼저 post인지 아닌지 검사하는 것이 필요할 것 같다
+    # TODO 먼저 post인지 아닌지 검사하는 것이 필요할 것 같다
     if form.validate_on_submit():
         user = User.query.filter_by(user_email=form.user_email.data).first()
         if user:
             flash('User ID is already exsits.')
             return redirect(request.path)
-        else:
-            user = User(
-                user_email=form.user_email.data,
-                password=security.generate_password_hash(form.password.data)
-            )
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            send_auth_email()
-            # login_user(user)
-        return redirect(url_for('data.file'))
+        
+        user = User(
+            user_email=form.user_email.data,
+            password=security.generate_password_hash(form.password.data)
+        )
+        db.session.add(user)
+        db.session.commit()
+        send_auth_email(user)
+        return render_template(f'{NAME}/register_email_info_page.html', user_email=form.user_email)
     else:
         flash_form_errors(form)
-    
-    if current_user.is_authenticated:
-        return redirect(url_for('data.file'))
 
     return render_template(f'{NAME}/register.html', form=form)
 
+
+@bp.route("/email/send/<string:user_email>/")
+def email_send(user_email):
+    user = User.query.filter_by(user_email=user_email).first()
+    send_auth_email(user)
+    return render_template(f'{NAME}/register_email_info_page.html', user_email=user.user_email)
+
+
 @bp.route("/email_auth_completed_page/")
 def email_auth_completed_page():
-    return render_template("email_auth_completed_page.html")
+    """이메일 인증 완료 페이지"""
+    if current_user.is_authenticated:
+        return redirect(url_for("data.file"))
+
+    token = request.args.get("token", None)
+    user_email=request.args.get('email')
+    user = User.query.filter_by(user_email=user_email).first()
+
+    if user and user.active:
+        login_user(user)
+        return redirect(url_for("data.file"))
+
+    if not token:
+        flash('이메일 인증 토큰 정보가 없습니다. 재시도 바랍니다.', 'errors')
+
+    expired, invalid, user = confirm_email_token_status(token)
+
+    if not user or invalid:
+        flash('이메일 인증 사용자 정보가 없습니다.', 'errors')
+    elif expired:
+        flash('이메일 인증 유효기간이 만료되었습니다.', 'errors')
+    else:
+        user.active = True
+        db.session.commit()
+        login_user(user)
+        
+
+    return render_template(f"{NAME}/email_auth_completed_page.html", user_email=request.args.get('email'))
--- utils/user.py
+++ utils/user.py
@@ -1,7 +1,8 @@
 from flask import current_app, render_template, url_for
+from werkzeug.utils import redirect
 from flask_mail import Message
 from flask_login import current_user
-from itsdangerous import URLSafeTimedSerializer
+from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
 
 from hashlib import sha1
 
@@ -14,16 +15,16 @@ from monolithic.tasks import mail
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-def send_auth_email():
+def send_auth_email(user):
     confirmation_link = generate_confirmation_link(
-        current_user, url_txt='auth.email_auth_completed_page'
+        user, url_txt='auth.email_auth_completed_page'
     )
 
-    context = dict(email_addr=current_user.user_email, confirmation_link=confirmation_link)
+    context = dict(email_addr=user.user_email, confirmation_link=confirmation_link)
 
     mail.send_email_celery.delay(
         subject='가입 이메일 인증',
-        recipient=current_user.user_email,
+        recipient=user.user_email,
         template='email/send_auth_email',
         **context
     )
@@ -40,3 +41,24 @@ def generate_confirmation_token(user):
     data = [str(user.id), sha1(user.user_email.encode('utf-8')).hexdigest()]
     secret_key = current_app.config.get('SECRET_KEY')
     return URLSafeTimedSerializer(secret_key=secret_key, salt=None).dumps(data)
+
+def confirm_email_token_status(token):
+    serializer = URLSafeTimedSerializer(secret_key=current_app.config.get('SECRET_KEY'), salt=None)
+    max_age = current_app.config.get('SECURITY_CONFIRM_EMAIL_WITHIN')
+    user, data = None, None
+    expired, invalid = False, False
+
+    try:
+        data = serializer.loads(token, max_age=max_age)
+    except SignatureExpired:
+        _, data = serializer.loads_unsafe(token)
+        expired = True
+    except (BadSignature, TypeError, ValueError):
+        invalid = True
+
+    if data:
+        user = User.query.filter_by(id=data[0]).one_or_none()
+
+    expired = expired and (user is not None)  # TODO 이게 꼭 필요한가...
+
+    return expired, invalid, user
