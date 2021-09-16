4b196a1d3e6429e82b7004b894e370c095b1645f
Independent-Dev <juju08217@daum.net>
2021-09-16 17:28:20 +0900

    celery cron 데모 제작

--- celery_worker.py
+++ celery_worker.py
@@ -2,3 +2,11 @@ from monolithic import celery, create_app, init_celery
 
 app = create_app()
 celery = init_celery(app)
+
+celery.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'monolithic.tasks.data.print_hello',
+        'schedule': 30.0,
+        # 'args': (16, 16)
+    },
+}
--- tasks/data.py
+++ tasks/data.py
@@ -4,7 +4,16 @@ from monolithic import mail, celery
 from monolithic.tasks.mail import send_email_celery
 from monolithic.utils.data import UpdateTracker
 
+# @celery.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Calls test('world') every 30 seconds
+#     sender.add_periodic_task(30.0, print_hello.s('world'), expires=10)
+
 @celery.task
 def analyze_and_report_package_data(package_info, user_email):
     update_tracker = UpdateTracker(package_info, user_email)
     update_tracker.report_package_info()
+
+@celery.task
+def print_hello():
+    current_app.logger.info("hello")
